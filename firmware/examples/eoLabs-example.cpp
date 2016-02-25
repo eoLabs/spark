2ad1e109b461bb9305190b7fe0fe3cda05d588c6
eoLabs <developer@eo-labs.com>
2016-02-25 11:34:53 -0600

    fixing errors for spark
    Resubmitting with errors fixed

---
+++ firmware/examples/eoLabs-example.cpp
@@ -0,0 +1,15 @@
+// IMPORTANT: When including a library in a firmware app, a sub dir prefix is needed
+// before the particular .h file.
+#include "eoLabs/eoLabs-example.h"
+
+
+void setup() {
+  // Call functions on initialized library objects that require hardware
+  // to be wired up correct and available.
+
+}
+
+void loop() {
+  // Use the library's initialized objects and functions
+
+}
