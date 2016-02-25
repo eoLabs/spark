4479ce8537e99957dd51f08993caf058a7934945
eoLabs <developer@eo-labs.com>
2016-02-25 14:48:45 -0600

    Fixed include path

--- firmware/eoLabs-library.h
+++ firmware/eoLabs-library.h
@@ -1,8 +1,7 @@
 #ifndef _EOLABS_LIBRARY
 #define _EOLABS_LIBRARY
 
-// TEMPORARY UNTIL the stuff that supports the code above is deployed to the build IDE
-#include "Keller.h"
+#include "Keller/Keller.h"
 
 
 #endif
